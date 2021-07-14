fc3079c242dd71eed02a0979190f696497ce40ff
mohsin <morteen1997@gmail.com>
2021-07-14 20:27:41 +0600

    release v1.1

--- README.md
+++ README.md
@@ -1,20 +1,47 @@
 # Covid-19 Tracker
 
 ## installation
-> coming soon...
+``` bash
+git clone https://github.com/mohsin-riad/Covid-19-Tracker.git
+cd Covid-19-Tracker
+npm install
+```
 ### Tailwind integration
 * setup: [Tailwind-css](https://tailwindcss.com/docs/installation)
-> coming soon...
+``` bash
+# Generate tailwind.config.js >full
+vue add tailwind 
+```
 
 ### Compiles and hot-reloads for development
-> coming soon...
+``` bash
+npm run serve
+```
 
 ## Build
-> coming soon...
-### Compiles and minifies for production
-> coming soon...
+> install Node Js(v8 engine) | dependency (npm)
+* setup: [NOdeJs LTS](https://nodejs.org/en/download/)
+``` bash
+# select: vue 3, vue router, bebel
+vue create Covid-19-Tracker
+
+# run localhost server
+npm run serve
+```
+
+### Api Package configuration [Axios]
+``` bash
+# individual project integration
+npm i axios --save
+```
+> Add to:  ``` src/main.js ```
+``` bash
+import axios from 'axios'
+```
+> used to sent POST and GET request to the server
+
 ### Api 
-> coming soon...
+> Open source api from [covid19api](https://covid19api.com/)
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,17 @@
 <template>
     <div :class="isDark ? 'dark' : '' ">
         <div class="bg-gray-100 dark:bg-gray-900 min-h-screen">
-            <div class="text-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 ... dark:bg-gradient-to-r dark:from-purple-900 dark:via-pink-900 dark:to-red-900 lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl md:max-w-screen-md max-w-md text-white p-4 w-full mx-auto mb-5 text-3xl md:text-5xl rounded">
+            <div class="text-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 ... dark:bg-gradient-to-r dark:from-purple-900 dark:via-pink-900 dark:to-red-900 lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl md:max-w-screen-md max-w-md text-gray-700 dark:text-gray-300 p-4 w-full mx-auto mb-5 text-3xl md:text-5xl rounded">
                 <i class="fas fa-lungs-virus animate-pulse"></i>Covid 19 Info
-                <button @click.prevent="isDark = !isDark" class="flex p-4 rounded-full mx-auto focus:outline-none hover:text-blue hover:bg-opacity-">
+                <!-- <button @click.prevent="isDark = !isDark" class="flex rounded-full mx-auto focus:outline-none hover:text-blue hover:bg-opacity-">
                     <div v-if="isDark"><i class="fas fa-toggle-on"></i></div>
                     <div v-else><i class="fas fa-toggle-off"></i></div>
-                </button>
+                </button> -->
+                <div class=" relative inline-block ml-10 w-10 mr-1 align-middle select-none transition duration-200 ease-in">
+                    <input @click="isDark = !isDark"  type="checkbox" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-gray-700 border-4 appearance-none cursor-pointer"/>
+                    <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+                </div>
+                <label class="dark:text-gray-300 text-xs font-bold align-middle text-gray-700">Dark Mode</label>
             </div> 
             <router-view/>
         </div>
@@ -22,3 +27,12 @@ export default {
     }
 }
 </script>
+<style scoped>
+.toggle-checkbox:checked {
+  right: 0;
+  border-color: #c3c3c3;
+}
+.toggle-checkbox:checked + .toggle-label {
+  background-color: #272727;
+}
+</style>
